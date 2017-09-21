6b6161774599723b6cf090c4dfc20e581257025c
Jose Antonini <jocantonini@gmail.com>
2017-09-20 23:55:46 -0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var {User}=require("./models/user.js");
 var app = express();
 
 app.use(bodyParser.json());
-
+//save a todo to the database route
 app.post("/todos",(req,res)=>{
     var todo = new Todo({
         text: req.body.text
@@ -21,6 +21,16 @@ app.post("/todos",(req,res)=>{
         res.status(400).send(e);
     });
 })
+//get all todos from the database route
+app.get("/todos",(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({
+            todos
+        })
+    },(e)=>{
+        res.status(400).send(e);
+    })
+});
 
 app.listen(3000,()=>{
     console.log("Svr started on port 3000");
