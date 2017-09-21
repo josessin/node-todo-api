d832b4c9a6c68e4796efa3e3e6b7364ac6d93f7e
Jose Antonini <jocantonini@gmail.com>
2017-09-21 01:40:14 -0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,39 +1,67 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var { ObjectID } = require("mongodb");
 
-var {mongoose}=require("./db/mongoose.js");
-var {mongoose} = require("./db/mongoose.js");
-var {Todo} = require("./models/todo.js");
-var {User}=require("./models/user.js");
+var { mongoose } = require("./db/mongoose.js");
+var { mongoose } = require("./db/mongoose.js");
+var { Todo } = require("./models/todo.js");
+var { User } = require("./models/user.js");
 
 var app = express();
 
 app.use(bodyParser.json());
 //save a todo to the database route
-app.post("/todos",(req,res)=>{
+app.post("/todos", (req, res) => {
     var todo = new Todo({
         text: req.body.text
     })
 
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    },(e)=>{
+    }, (e) => {
         res.status(400).send(e);
     });
 })
 //get all todos from the database route
-app.get("/todos",(req,res)=>{
-    Todo.find().then((todos)=>{
-        res.send({
-            todos
-        })
-    },(e)=>{
+app.get("/todos", (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
         res.status(400).send(e);
     });
 });
 
-app.listen(3000,()=>{
+//get a todo by id route
+app.get("/todos/:id", (req, res) => {
+    //acces de :id throguh the req
+    var id = req.params.id;
+    //validate id
+    if (!ObjectID.isValid(id)) {
+        console.log("GET /todos/:id: Requested id is invalid");
+        res.status(404).send();
+        return;
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            console.log("GET /todos/:id: No object with the given id");
+            res.status(404).send();
+            return;
+        }
+
+        res.send({todo});
+
+    }).catch((e) => {
+        console.log(e);
+        res.status(400).send();
+    })
+
+});
+
+app.listen(3000, () => {
     console.log("Svr started on port 3000");
 })
 
-module.exports = {app};
+
+
+module.exports = { app };
