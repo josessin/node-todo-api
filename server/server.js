7eae9471b9330b637d3a17ae3b651146d833d0f6
Jose Antonini <jocantonini@gmail.com>
2017-09-20 21:41:07 -0300

    add save to mongodb

--- server/server.js
+++ server/server.js
@@ -11,7 +11,15 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post("/todos",(req,res)=>{
-    console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(e)=>{
+        res.status(400).send(e);
+    });
 })
 
 app.listen(3000,()=>{
